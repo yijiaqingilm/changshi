e6eb98cd79f1af86d2f0803b81506a60eda82de8
what <jqyi@@3ti.us>
2018-07-10 21:34:21 +0800

    update code

--- src/pages/training/video/VideoMain.vue
+++ src/pages/training/video/VideoMain.vue
@@ -7,12 +7,14 @@
         <section>
             <header>
                 <div class='v-h-title'>
-                    已观看时间：xxx分钟
+                    已观看时间：{{currentTime}}分钟
                 </div>
                 <div>
-                    <d-player class='' ref="player" container=""></d-player>
+                    <div class='dplayer'></div>
                 </div>
             </header>
+            <div @click="hidePauseBtn">暂停</div>
+            <div @click="play()">播放</div>
             <section v-if='isShow' class='subject'>
                 <div>答题进度：1 -9</div>
                 <header class='s-title'>1.xxx</header>
@@ -36,29 +38,71 @@
 </template>
 
 <script>
-  import VueDPlayer from 'vue-dplayer'
-
+  // import VueDPlayer from 'vue-dplayer'
+  import DPlayer from 'dplayer'
   export default {
     name: 'videoMain',
     data () {
       return {
-        isShow: false
+        isShow: false,
+        dp: null,
+        currentTime: 0,
+        step: 2
+
       }
     },
+    created(){
+    },
     methods: {
+      pause(){
+        this.dp.pause()
+      },
+      hidePauseBtn(){
+        this.dp.pause()
+        this.$$('.dplayer  .dplayer-play-icon').hide()
+      },
       play () {
-        console.log('play callback')
+        this.$$('.dplayer  .dplayer-play-icon').show()
+        this.dp.play()
+      },
+      showSubject(){
+        this.hidePauseBtn()
       }
     },
     mounted () {
-      const player = this.$refs.player.dp
-      console.log('this.', this.$refs.player)
-      player.play()
-      setTimeout(() => {
-        player.pause()
-      }, 2000)
+      this.$nextTick(() => {
+        this.dp = new DPlayer({
+          container: document.querySelector('.dplayer'),
+          autoplay: false,
+          theme: '#FADFA3',
+          loop: true,
+          lang: 'zh-cn',
+          screenshot: true,
+          hotkey: true,
+          preload: 'auto',
+          logo: 'logo.png',
+          volume: 0.7,
+          mutex: true,
+          video: {
+            url: 'https://lightsns.oss-cn-qingdao.aliyuncs.com/demo_video.mp4',
+            pic: require('../../../assets/icon_change.png'),
+            type: 'auto'
+          },
+        })
+        this.$$('.dplayer  .dplayer-setting').hide()
+        this.$$('.dplayer .dplayer-full').hide()
+        this.dp.on('timeupdate', () => {
+          this.currentTime = Math.floor(this.dp.video.currentTime / 60)
+        });
+      })
     },
-    components: {'d-player': VueDPlayer}
+    watch: {
+      currentTime(nowTime, oldTime){
+        if (nowTime % this.step === 0) {
+          this.showSubject()
+        }
+      }
+    }
   }
 </script>
 
